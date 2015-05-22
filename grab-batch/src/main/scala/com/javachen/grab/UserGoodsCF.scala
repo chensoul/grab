6e128b60142291d35072162fda51663c5481ff5c
javachen <june.chan@foxmail.com>
2015-05-22 10:59:39 +0800

    修改协同过滤代码逻辑

--- grab-batch/src/main/scala/com/javachen/grab/UserGoodsCF.scala
+++ grab-batch/src/main/scala/com/javachen/grab/UserGoodsCF.scala
@@ -1,112 +1,121 @@
 package com.javachen.grab
 
-import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
-import org.apache.spark.rdd._
+import org.apache.spark.mllib.recommendation.{ALS, Rating}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
-import sys.process._
-
-//import org.apache.log4j.{Logger,Level}
+import scala.Option
+import scala.collection.mutable
+import scala.sys.process._
+import org.apache.log4j.{Logger,Level}
 
 /**
- *
- * @author <a href="mailto:junechen@163.com">june</a>.
- * @date 2015-05-19 10:18.
+ * 基于物品的协同过滤给用户进行推荐
  */
 object UserGoodsCF {
   def main(args: Array[String]): Unit = {
-    //Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    //Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
+
 
     // set up environment
-    val sc = new SparkContext(new SparkConf().setAppName("UserGoodsCF"))
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val conf = new SparkConf().setAppName("UserGoodsCF")
+    conf.registerKryoClasses(Array(classOf[mutable.BitSet], classOf[Rating])).set("spark.kryoserializer.buffer.mb", "100")
+    conf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+    conf.set("spark.akka.frameSize", "500m");
 
-    sqlContext.parquetFile("/logroot/user_goods_preference").registerTempTable("user_goods_preference")
-    //user_id: int, goods_id: int, visit_count: int, visit_time: int, visit_city_id: int, buy_count: int, buy_time: int, buy_city_id: int, avg_score: float, collect_count: int
-    val user_goods_preference = sqlContext.sql("SELECT * FROM user_goods_preference where user_id>0 limit 1000")
-    val user_citys=user_goods_preference.map(t=> (t(0).asInstanceOf[Int],t(4).asInstanceOf[Int])).distinct()
+    val sc = new SparkContext(conf)
+    val hc = new HiveContext(sc)
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
-    //0,7,235,1431943216,0,null,null,null,null,null
-    val ratings = user_goods_preference.map { t =>
+    val userGoodsRDD = hc.sql("SELECT * FROM dw_recommender.user_goods_preference where user_id>0")
+    val ratings = userGoodsRDD.map { t =>
       var score = 0.0
-      if (t(2).asInstanceOf[Int] > 0)
-        score += 0.7 //访问
+      if (t(2).asInstanceOf[Int] > 0) score += 5 * 0.7 //访问
+      if (t(5).asInstanceOf[Int] > 0) score += 5 * 0.2 //购买
+      if (t(9).asInstanceOf[Int] > 0) score += 5 * 0.1 //搜藏
+      if (t(8).asInstanceOf[Double] > 0)
+        score = t(8).asInstanceOf[Double] * 0.9 //评论
+      Rating(t(0).asInstanceOf[Int], t(1).asInstanceOf[Int], score)
+    }.cache()
 
-      if (t(5).asInstanceOf[Int] > 0)
-        score += 0.2 //购买
+    val numRatings = ratings.count()
+    val numUsers = ratings.map(_.user).distinct().count()
+    val numGoods = ratings.map(_.product).distinct().count()
+    println(s"Got $numRatings ratings from $numUsers users on $numGoods goods.")
 
-      if (t(8).asInstanceOf[Float] > 0)
-        score += t(8).asInstanceOf[Float] * 0.9 //评分
-
-      if (t(9).asInstanceOf[Int] > 0)
-        score += 0.1 //搜藏
-
-      (t(3).asInstanceOf[Int] % 10, Rating(t(0).asInstanceOf[Int], t(1).asInstanceOf[Int], score))
-    }
-
-    //ratings.collect().foreach(println)
-
-    println("Got " + ratings.count() + " ratings from "
-      +  ratings.map(_._2.user).distinct().count() + " users on " + ratings.map(_._2.product).distinct().count() + "goods.")
-
+    //1.训练模型
     val rank = 12
     val numIterations = 20
     val lambda = 0.01
     val numPartitions = 4
+    var start = System.currentTimeMillis()
 
-    val training = ratings.values.repartition(numPartitions).cache()
+    val training = ratings.repartition(numPartitions)
     val model = ALS.train(training, rank, numIterations, lambda)
+    println("Train Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
 
-    val usersProducts = training.map { case Rating(user, product, rate) =>
+    // 2.预测评分
+    val usersProducts = ratings.map { case Rating(user, product, rate) =>
       (user, product)
     }
 
+    //每个用户取前40个预测结果
     val predictions = model.predict(usersProducts).map { case Rating(user, product, rate) =>
-      ((user, product), rate)
-    }
+        (user, (product, rate))}.groupByKey(4).map(t=>(t._1,t._2.toList.sortBy(x=> - x._2).take(40).map{case (goods_id,rate)=>goods_id}))
 
-    val ratesAndPreds = training.map { case Rating(user, product, rate) =>
-      ((user, product), rate)
-    }.join(predictions).sortByKey()
+    val hot_goods_sql="select city_id,goods_id,total_amount from dw_recommender.hot_goods"
+    val send_goods_sql="select goods_id from dw_recommender.hot_goods where city_id=9999 order by total_amount desc"
+    val goods_sp_sql="select goods_id,sp_id from dw_recommender.online_goods"
+    val goodsSpRDD = hc.sql(goods_sp_sql).map({t =>(t(0).asInstanceOf[Int],t(1).asInstanceOf[Int])})
+    val userCityRDD = userGoodsRDD.map(t => (t(0).asInstanceOf[Int], t(4).asInstanceOf[Int]))
+    //每个城市热销商品取20个
+    val hotGoodsRDD = hc.sql(hot_goods_sql).map(t => (t(0).asInstanceOf[Int],(t(1).asInstanceOf[Int],t(2).asInstanceOf[Int]))).groupByKey(4).map(t=>
+      (t._1,t._2.toList.sortBy(x=> - x._2).take(20).map{case (goods_id,total_num)=>goods_id} ))
+    //配送商品取10个
+    val sendGoods = sc.broadcast(hc.sql(send_goods_sql).map(t =>(t(0).asInstanceOf[Int])).take(10)).value
 
-    val RMSE = math.sqrt(ratesAndPreds.map { case ((user, product), (r1, r2)) =>
-      val err = (r1 - r2)
-      err * err
-    }.mean())
 
-    println(s"The model was trained with rank = $rank and lambda = $lambda, and numIter = $numIterations, and its RMSE is $RMSE.")
+    //3.合并结果：推荐40个+热销商品20个+配送商品10个
+    start = System.currentTimeMillis()
+    var userRecommendations=predictions.join(userCityRDD).map({case (user_id,(list1,city_id))=>
+      (city_id,(user_id,list1))
+    }).join(hotGoodsRDD).map({case (city_id,((user_id,list1),list2))=>
+      var list=list1
+      list ++= list2
+      list ++= sendGoods
 
-
-    //确保只生成一个文件，并按用户排序
-    val formatedRatesAndPreds = ratesAndPreds.repartition(1).sortBy(_._1).map({
-      case ((user, product), (rate, pred)) => user + "\t" + product + "\t" + rate + "\t" + pred
+      (user_id,city_id,list)
     })
 
+    //4.去重
+
+    println("Not exist User Recommendation Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
+
     "hadoop fs -rm -r /tmp/user_goods_rates".!
-    formatedRatesAndPreds.saveAsTextFile("/tmp/user_goods_rates")
+    System.setProperty("spark.akka.frameSize","1024m")
+//    sc.parallelize(sc.broadcast(userRecommendations).value,1).saveAsTextFile("/tmp/user_goods_rates")
 
-    //排序取10条，限制结果集为5，待确认代码是否正确
-    predictions.map( { case  ((user, product), rate)=> Rating(user, product, rate)}).sortBy(- _.rating).groupBy(_.user).map(x => x._2.take(2)).take(5)
+    //5.保存推荐结果到redis
 
-//    val result=user_citys.collect().map(t=> {
-//      try {
-//        val rs = model.recommendProducts(t._1, 10)
-//        var value = ""
-//
-//        rs.foreach(r => {
-//          //value = value + r.product + ":" + r.rating + ","
-//          value += r.product + ","
-//        })
-//        Array(t._1, t._2, value.substring(0, value.length - 2)).mkString("\t")
-//      }catch{
-//          case ex: java.util.NoSuchElementException =>{}
-//      }
-//    })
-//
-//    val resultRDD = sc.parallelize(result,1)//并行为1，hdfs上只生成一个文件
-//    resultRDD.saveAsTextFile("/tmp/user_goods_result")
+    training.unpersist()
+    userGoodsRDD.unpersist()
 
-     }
+    sc.stop()
+  }
+
+  /**
+   * 去重，一个商家只推一个商品，并取40个结果
+   * @param sc
+   * @param recommendations
+   * @param goodsSpRDD
+   * @return
+   */
+  def removeDupplicate(sc:SparkContext,recommendations:List[Int],goodsSpRDD:RDD[(Int, Int)]): String ={
+    recommendations.mkString(",")
+//    sc.parallelize(recommendations).map(goods_id=>(goods_id,1)).join(goodsSpRDD).map({
+//      case (goods_id,(1, sp_id)) => (sp_id, goods_id)
+//    }).groupBy(_._1).map({t=>t._2.take(1)}).map(_.head._2).collect().take(40).mkString(",")
+  }
 
 }
--- grab-batch/src/main/scala/com/javachen/grab/UserGoodsCFTest.scala
+++ grab-batch/src/main/scala/com/javachen/grab/UserGoodsCFTest.scala
@@ -2,54 +2,43 @@ package com.javachen.grab
 
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
 import org.apache.spark.rdd._
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
+ * 基于物品的协同过滤，根据不同输入参数计算最佳训练模型
  *
  * @author <a href="mailto:junechen@163.com">june</a>.
- * @date 2015-05-19 10:18.
+ *         2015-05-19 10:18
  */
 object UserGoodsCFTest {
   def main(args: Array[String]): Unit = {
+    //import org.apache.log4j.{Logger,Level}
     //Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     //Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
     // set up environment
     val sc = new SparkContext(new SparkConf().setAppName("UserGoodsCFTest"))
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val hc = new HiveContext(sc)
 
-    sqlContext.parquetFile("/logroot/user_goods_preference").registerTempTable("user_goods_preference")
-    val user_goods_preference = sqlContext.sql("SELECT * FROM user_goods_preference where user_id>0 limit 10")
+    val userGoodsRDD = hc.sql("SELECT * FROM dw_recommender.user_goods_preference where user_id>0").cache
 
-    //user_goods_preference.map(t=>(t(0),t(1))).saveAsTextFile("/tmp/user_goods_preference")
-
-    //user_goods_preference.collect().foreach(println)
-
-    //0,7,235,1431943216,0,null,null,null,null,null
-    val ratings = user_goods_preference.map { t =>
+    val ratings = userGoodsRDD.map { t =>
       var score = 0.0
-      if (t(2).asInstanceOf[Int] > 0)
-        score += 0.7 //访问
+      if (t(2).asInstanceOf[Int] > 0) score += 5 * 0.7 //访问
+      if (t(5).asInstanceOf[Int] > 0) score += 5 * 0.2 //购买
+      if (t(9).asInstanceOf[Int] > 0) score += 5 * 0.1 //搜藏
+      if (t(8).asInstanceOf[Double] > 0)
+        score = t(8).asInstanceOf[Double] * 0.9 //评论
 
-      if (t(5).asInstanceOf[Int] > 0)
-        score += 0.2 //购买
-
-      if (t(8).asInstanceOf[Float] > 0)
-        score += t(8).asInstanceOf[Float] * 0.9 //评分
-
-      if (t(9).asInstanceOf[Int] > 0)
-        score += 0.1 //搜藏
-
-      (t(3).asInstanceOf[Int] % 10, Rating(t(0).asInstanceOf[Int], t(1).asInstanceOf[Int], score))
+      (t(3).asInstanceOf[Int],Rating(t(0).asInstanceOf[Int], t(1).asInstanceOf[Int], score))
     }
-
-    //ratings.collect().foreach(println)
 
     val numRatings = ratings.count()
     val numUsers = ratings.map(_._2.user).distinct().count()
     val numGoods = ratings.map(_._2.product).distinct().count()
 
-    println("Got " + numRatings + " ratings from " + numUsers + " users on " + numGoods + "goods.")
+    println(s"Got $numRatings ratings from $numUsers users on $numGoods movies.")
 
     // split ratings into train (60%), validation (20%), and test (20%) based on the
     // last digit of the timestamp, add myRatings to train, and cache them
@@ -63,11 +52,11 @@ object UserGoodsCFTest {
     val numValidation = validation.count()
     val numTest = test.count()
 
-    println("Training: " + numTraining + ", validation: " + numValidation + ", test: " + numTest)
+    println(s"Training: $numTraining, validation: $numValidation, test: $numTest")
 
     // train models and evaluate them on the validation set
-    val ranks = List(8, 12)
-    val lambdas = List(0.1, 10.0)
+    val ranks = List(8, 12, 16)
+    val lambdas = List(0.01, 0.1, 10.0)
     val numIterations = List(10, 20)
     var bestModel: Option[MatrixFactorizationModel] = None
     var bestValidationRmse = Double.MaxValue
@@ -77,8 +66,8 @@ object UserGoodsCFTest {
     for (rank <- ranks; lambda <- lambdas; numIter <- numIterations) {
       val model = ALS.train(training, rank, numIter, lambda)
       val validationRmse = computeRmse(model, validation)
-      println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
-        + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".")
+      println(s"RMSE (validation) = $validationRmse for the model trained with rank = $rank , lambda = $lambda , and numIter = $numIter .")
+
       if (validationRmse < bestValidationRmse) {
         bestModel = Some(model)
         bestValidationRmse = validationRmse
@@ -91,11 +80,9 @@ object UserGoodsCFTest {
     // evaluate the best model on the test set
     val testRmse = computeRmse(bestModel.get, test)
 
-    println("The best model was trained with rank = " + bestRank + " and lambda = " + bestLambda
-      + ", and numIter = " + bestNumIter + ", and its RMSE on the test set is " + testRmse + ".")
+    println(s"The best model was trained with rank = $bestRank and lambda = $bestLambda , and numIter = $bestNumIter , and its RMSE on the test set is $testRmse .")
 
     // create a naive baseline and compare it with the best model
-
     val meanRating = training.union(validation).map(_.rating).mean
     val baselineRmse =
       math.sqrt(test.map(x => (meanRating - x.rating) * (meanRating - x.rating)).mean)
