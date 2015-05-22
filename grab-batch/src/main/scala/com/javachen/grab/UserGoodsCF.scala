c8cf8c31e78f0702a7269b62f44edb635eca4ab8
javachen <june.chan@foxmail.com>
2015-05-22 11:36:12 +0800

    修改协同过滤代码逻辑

--- grab-batch/src/main/scala/com/javachen/grab/UserGoodsCF.scala
+++ grab-batch/src/main/scala/com/javachen/grab/UserGoodsCF.scala
@@ -11,12 +11,10 @@ import scala.sys.process._
 import org.apache.log4j.{Logger,Level}
 
 /**
- * 基于物品的协同过滤给用户进行推荐
+ * 基于物品的协同过滤
  */
 object UserGoodsCF {
   def main(args: Array[String]): Unit = {
-
-
     // set up environment
     val conf = new SparkConf().setAppName("UserGoodsCF")
     conf.registerKryoClasses(Array(classOf[mutable.BitSet], classOf[Rating])).set("spark.kryoserializer.buffer.mb", "100")
@@ -28,7 +26,8 @@ object UserGoodsCF {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
-    val userGoodsRDD = hc.sql("SELECT * FROM dw_recommender.user_goods_preference where user_id>0")
+    //表结构：user_id, goods_id, visit_count, visit_time, visit_city_id, buy_count, buy_time, buy_city_id, avg_score, collect_count
+    val userGoodsRDD = hc.sql("select * from dw_recommender.user_goods_preference where user_id>0")
     val ratings = userGoodsRDD.map { t =>
       var score = 0.0
       if (t(2).asInstanceOf[Int] > 0) score += 5 * 0.7 //访问
@@ -66,17 +65,19 @@ object UserGoodsCF {
 
     val hot_goods_sql="select city_id,goods_id,total_amount from dw_recommender.hot_goods"
     val send_goods_sql="select goods_id from dw_recommender.hot_goods where city_id=9999 order by total_amount desc"
-    val goods_sp_sql="select goods_id,sp_id from dw_recommender.online_goods"
-    val goodsSpRDD = hc.sql(goods_sp_sql).map({t =>(t(0).asInstanceOf[Int],t(1).asInstanceOf[Int])})
+    val online_goods_sql="select goods_id,sp_id from dw_recommender.online_goods"
+
+    val onlineGoodsRDD = hc.sql(online_goods_sql).map({t =>(t(0).asInstanceOf[Int],t(1).asInstanceOf[Int])})
     val userCityRDD = userGoodsRDD.map(t => (t(0).asInstanceOf[Int], t(4).asInstanceOf[Int]))
     //每个城市热销商品取20个
     val hotGoodsRDD = hc.sql(hot_goods_sql).map(t => (t(0).asInstanceOf[Int],(t(1).asInstanceOf[Int],t(2).asInstanceOf[Int]))).groupByKey(4).map(t=>
       (t._1,t._2.toList.sortBy(x=> - x._2).take(20).map{case (goods_id,total_num)=>goods_id} ))
-    //配送商品取10个
-    val sendGoods = sc.broadcast(hc.sql(send_goods_sql).map(t =>(t(0).asInstanceOf[Int])).take(10)).value
+    //配送商品取20个
+    val sendGoods = sc.broadcast(hc.sql(send_goods_sql).map(t =>(t(0).asInstanceOf[Int])).take(20)).value
+    val onlineGoods= sc.broadcast(onlineGoodsRDD.collectAsMap()).value
 
 
-    //3.合并结果：推荐40个+热销商品20个+配送商品10个
+    //3.合并结果：推荐40个+热销商品20个+配送商品20个，并过滤下线的商品
     start = System.currentTimeMillis()
     var userRecommendations=predictions.join(userCityRDD).map({case (user_id,(list1,city_id))=>
       (city_id,(user_id,list1))
@@ -85,21 +86,19 @@ object UserGoodsCF {
       list ++= list2
       list ++= sendGoods
 
-      (user_id,city_id,list)
+      (user_id,city_id,list.filter(onlineGoods.contains(_))) //取在线商品
     })
 
-    //4.去重
+    //4.去重：一个商家只推一个商品，最后取前40个结果
 
     println("Not exist User Recommendation Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
 
     "hadoop fs -rm -r /tmp/user_goods_rates".!
-    System.setProperty("spark.akka.frameSize","1024m")
-//    sc.parallelize(sc.broadcast(userRecommendations).value,1).saveAsTextFile("/tmp/user_goods_rates")
+    //userRecommendations.saveAsTextFile("/tmp/user_goods_rates")
 
     //5.保存推荐结果到redis
 
-    training.unpersist()
-    userGoodsRDD.unpersist()
+    ratings.unpersist()
 
     sc.stop()
   }
@@ -108,14 +107,13 @@ object UserGoodsCF {
    * 去重，一个商家只推一个商品，并取40个结果
    * @param sc
    * @param recommendations
-   * @param goodsSpRDD
+   * @param onlineGoodsRDD
    * @return
    */
-  def removeDupplicate(sc:SparkContext,recommendations:List[Int],goodsSpRDD:RDD[(Int, Int)]): String ={
-    recommendations.mkString(",")
-//    sc.parallelize(recommendations).map(goods_id=>(goods_id,1)).join(goodsSpRDD).map({
-//      case (goods_id,(1, sp_id)) => (sp_id, goods_id)
-//    }).groupBy(_._1).map({t=>t._2.take(1)}).map(_.head._2).collect().take(40).mkString(",")
+  def removeDupplicate(sc:SparkContext,recommendations:List[Int],onlineGoodsRDD:RDD[(Int, Int)]): String ={
+    sc.parallelize(recommendations).map(goods_id=>(goods_id,1)).join(onlineGoodsRDD).map({
+      case (goods_id,(1, sp_id)) => (sp_id, goods_id)
+    }).groupBy(_._1).map({t=>t._2.take(1)}).map(_.head._2).collect().take(40).mkString(",")
   }
 
 }
