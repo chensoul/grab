1a0f338000af00941ce73b64d4a3adfa55bc75a7
javachen <june.chan@foxmail.com>
2015-06-05 14:45:20 +0800

    up

--- grab-batch/src/main/scala/com/javachen/grab/UserGoodsCF.scala
+++ grab-batch/src/main/scala/com/javachen/grab/UserGoodsCF.scala
@@ -7,8 +7,8 @@ import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 import org.jblas.DoubleMatrix
+import scala.sys.process._
 
-import scala.collection
 
 /**
  * 基于物品的协同过滤
@@ -18,84 +18,113 @@ object UserGoodsCF {
     // 1. 环境设置
     val conf = new SparkConf().setAppName("UserGoodsCF")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    conf.set("spark.kryoserializer.buffer.mb", "64")
+    //conf.set("spark.kryoserializer.buffer.mb", "64")
     conf.set("spark.kryo.classesToRegister", "scala.collection.mutable.BitSet,scala.Tuple2,scala.Tuple1,org.apache.spark.mllib.recommendation.Rating")
-    conf.set("spark.akka.frameSize", "500");
+    conf.set("spark.akka.frameSize", "50");//单位是MB
     val sc = new SparkContext(conf)
 
-    val hc = new HiveContext(sc)
-    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.spark").setLevel(Level.INFO)
     Logger.getLogger("org.apache.hadoop").setLevel(Level.WARN)
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
+    val hc = new HiveContext(sc)
+
     // 2. 加载数据
-    //表结构：user_id,goods_id,visit_time,city_id,visit_count,buy_count,is_collect,avg_score
-    val ratings = hc.sql("select * from dw_rec.user_goods_preference_filter").map { t =>
+    val ratings = hc.sql("select user_id,goods_id,visit_count,buy_count,is_collect,avg_score from dw_rec.user_goods_preference").map { t =>
       var score = 0.0
-      if (t(7).asInstanceOf[Double] > 0) {
-        score = t(7).asInstanceOf[Double] //评论
+      if (t(5).asInstanceOf[Double] > 0) {
+        score = t(5).asInstanceOf[Double] //评论
       } else {
-        if (t(4).asInstanceOf[Int] > 0) score += 5 * 0.3 //访问
-        if (t(5).asInstanceOf[Int] > 0) score += 5 * 0.6 //购买
-        if (t(6).asInstanceOf[Byte] > 0) score += 5 * 0.1 //搜藏
+        if (t(2).asInstanceOf[Int] > 0) score += 5 * 0.3 //访问
+        if (t(3).asInstanceOf[Int] > 0) score += 5 * 0.6 //购买
+        if (t(4).asInstanceOf[Byte] > 0) score += 5 * 0.1 //搜藏
       }
-      //((user,city),Rating)
-      ((t(0).asInstanceOf[Int], t(3).asInstanceOf[Int]), Rating(t(0).asInstanceOf[Int], t(1).asInstanceOf[Int], score))
-    }.repartition(128)
+      Rating(t(0).asInstanceOf[Int], t(1).asInstanceOf[Int], score)
+    }.repartition(4)
 
-    val userCitys = ratings.keys.groupByKey().map { case (user, list) => (user, list.last) }.collect() //对每个用户取最近的一个城市
-    val training = ratings.values.setName("training").persist(StorageLevel.MEMORY_AND_DISK_SER)
+    val training = ratings.setName("training").persist(StorageLevel.MEMORY_AND_DISK_SER)
 
-    val trainingUsers = training.map(_.user).distinct(4)
-    val trainingGoods = training.map(_.product).distinct(4)
+    val trainingUsers = training.map(_.user).distinct()
+    val trainingGoods = training.map(_.product).distinct()
     var start = System.currentTimeMillis()
     println("Got " + training.count() + " ratings from " + trainingUsers.count + " users on " + trainingGoods.count + " goods. ")
     println("Count Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
 
     // 3.训练模型
-    val rank = 12
-    val numIterations = 20
-    val lambda = 0.01
+    val (rank,numIterations,lambda) = (12,20,0.01)
     start = System.currentTimeMillis()
-    val model = ALS.trainImplicit(training, rank, numIterations, lambda, 1.0)
+    val model = ALS.train(training, rank, numIterations, lambda)
     println("Train Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
 
-    // 4.批量推荐，补齐数据：推荐数据40+热销数据30+配送数据30
+    training.unpersist()
+
+    // 4.批量推荐
+
+    //对每个用户取最近的一个城市
+    val userCitys = hc.sql("select distinct user_id,city_id from dw_rec.user_goods_preference").map { t =>
+      (t(0).asInstanceOf[Int], t(1).asInstanceOf[Int])
+    }.reduceByKey((x,y)=>y).collectAsMap()
+    val userCitysBD=sc.broadcast(userCitys)
+
+    //在线商品
     val onlineGoodsSp = hc.sql("select goods_id,sp_id from dw_rec.online_goods").map { t =>
       (t(0).asInstanceOf[Int], t(1).asInstanceOf[Int])
     }.collectAsMap()
-    val hotCityGoods: collection.Map[Int, Array[Int]] = hc.sql("select city_id,goods_id,total_amount from dw_rec.hot_goods").map { t =>
+    val onlineGoodsSpBD=sc.broadcast(onlineGoodsSp)
+
+    //热销商品，按城市分组统计
+    val hotCityGoods = hc.sql("select city_id,goods_id,total_amount from dw_rec.hot_goods").map { t =>
       (t(0).asInstanceOf[Int], (t(1).asInstanceOf[Int], t(2).asInstanceOf[Int]))
     }.groupByKey().map { case (city, list) =>
       (city, list.toArray.sortBy { case (goods_id, total_num) => -total_num }.take(30).map { case (goods_id, total_num) => goods_id })
     }.collectAsMap()
-    val seedGoods=hotCityGoods(9999)
+    val hotCityGoodsBD=sc.broadcast(hotCityGoods)
+    val seedGoodsBD=sc.broadcast(hotCityGoods(9999)) //配送商品 city_id=9999
 
-    val itemFactors = model.productFeatures.map { case (prodcut, factor) => factor }.collect()
+    val itemFactors = model.productFeatures.values.collect()
     val itemMatrix = new DoubleMatrix(itemFactors)
     val imBroadcast = sc.broadcast(itemMatrix)
-    println(itemMatrix.rows, itemMatrix.columns)
-
-    val idxProducts=model.productFeatures.map { case (prodcut, factor) => prodcut }.zipWithIndex().map{case (prodcut, idx) => (idx,prodcut)}.collectAsMap()
-    val idxProductsBroadcast = sc.broadcast(idxProducts)
+    val idxItems=model.productFeatures.keys.zipWithIndex().map{case (array, idx) => (idx,array)}.collectAsMap()
+    val idxItemsBroadcast = sc.broadcast(idxItems)
 
     val allRecs = model.userFeatures.map{ case (user, array) =>
       val userVector = new DoubleMatrix(array)
       val scores = imBroadcast.value.mmul(userVector)
       val sortedWithId = scores.data.zipWithIndex.sortBy(-_._1)
-      val recommendedProducts = sortedWithId.map(_._2).map{idx=>idxProductsBroadcast.value.get(idx).get}
-      (user, recommendedProducts)  //recommendedIds 为索引
+      //通过评分索引去找到对应的商品
+      val recommendedProducts = sortedWithId.take(40).map{case (score,idx)=>idxItemsBroadcast.value.get(idx).get}
+
+      //补齐数据：推荐数据40+热销数据30+配送数据30，去重（一个商家只推一个商品）之后再取前40
+      val city=userCitysBD.value.get(user).get
+      val hotGoods =hotCityGoodsBD.value.getOrElse(city,Array[Int]())
+      val toFilterGoods=recommendedProducts ++ hotGoods ++ seedGoodsBD.value
+      (user,city,filterGoods(toFilterGoods,onlineGoodsSpBD.value).mkString(","))
     }
 
-    // 5. 对于没推荐的用户给推荐热销数据加上配送数据
+//    var res=model.userFeatures.coalesce(4).cartesian(model.productFeatures.values.coalesce(4)).map{ case ((user, userArray), itemArray) =>
+//      val itemMatrix = new DoubleMatrix(itemArray)
+//      val userVector = new DoubleMatrix(userArray)
+//      val scores = itemMatrix.mmul(userVector)
+//      val sortedWithId = scores.data.zipWithIndex.sortBy(-_._1)
+//      val recommendedProducts = sortedWithId.map(_._2).take(10).map{idx=>idxProductsBroadcast.value.get(idx).get}.mkString(",")
+//      (user, recommendedProducts)
+//    }
+
+//    model.recommendProducts(10,10).map(_.product)
+
+    "hadoop fs -rm -r /tmp/allRecs".!
+
     start = System.currentTimeMillis()
-    hotCityGoods.map { case (city,list) =>
-      val toFilterGoods=list ++ seedGoods
-      (0, city, filterGoods(toFilterGoods, onlineGoodsSp))
-    }
+    allRecs.saveAsTextFile("/tmp/allRecs")
     println("Recommend Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
 
-    unpersist(model)
+    // 5. 对于没推荐的用户给推荐热销数据加上配送数据
+//    start = System.currentTimeMillis()
+//    hotCityGoods.map { case (city,list) =>
+//      val toFilterGoods=list ++ seedGoodsBD.value
+//      (0, city, filterGoods(toFilterGoods, onlineGoodsSp))
+//    }
+//    println("Recommend Time = " + (System.currentTimeMillis() - start) * 1.0 / 1000)
 
     sc.stop()
 
@@ -105,7 +134,7 @@ object UserGoodsCF {
   /**
    * 过滤商品，取在线商品并且一个商家只推一个商品
    */
-  def filterGoods(toFilterGoods: Array[Int], onlineGoodsSp: scala.collection.Map[Int, Int]): String = {
+  def filterGoods(toFilterGoods: Array[Int], onlineGoodsSp: scala.collection.Map[Int, Int]) = {
     var filtered = scala.collection.mutable.Map[Int, Int]()
     var sp_id = -1
 
@@ -116,14 +145,7 @@ object UserGoodsCF {
         filtered += sp_id -> product
       }
     }
-    filtered.values.mkString(",")
-  }
-
-  def unpersist(model: MatrixFactorizationModel): Unit = {
-    // At the moment, it's necessary to manually unpersist the RDDs inside the model
-    // when done with it in order to make sure they are promptly uncached
-    model.userFeatures.unpersist()
-    model.productFeatures.unpersist()
+    filtered.values
   }
 
   /** Compute RMSE (Root Mean Squared Error). */
